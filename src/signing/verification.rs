1fd09dc464e91562d5e30ff7d9efcb63f766d3b5
l30 <54937242+lamswe@users.noreply.github.com>
2023-03-13 13:58:08 +0000

    Update verification.rs

--- src/signing/verification.rs
+++ src/signing/verification.rs
@@ -1,31 +1,28 @@
-use curve25519_dalek::constants::RISTRETTO_BASEPOINT_TABLE;
+use curve25519_dalek::constants::{RISTRETTO_BASEPOINT_TABLE};
 use curve25519_dalek::ristretto::RistrettoPoint;
 use curve25519_dalek::scalar::Scalar;
 
 use super::header::*;
 use super::keyAgg::key_agg;
 
-pub fn ver(
-    m: String,
-    big_y: RistrettoPoint,
-    signature: (RistrettoPoint, Scalar),
-    committee: Committee,
-) -> bool {
+pub fn ver(m: String, big_y: RistrettoPoint, signature: (RistrettoPoint, Scalar), committee:Committee) -> bool {
     let c = hash_sig(big_y, signature.0, m);
     // println!("c in verfication: {:?}", c);
 
     let tilde_y = key_agg(committee).unwrap();
 
-    let rhs = (tilde_y + big_y) * c + signature.0; //(tilde_y + big_y)* c + signature.0;
+    let rhs = (tilde_y + big_y)* c + signature.0;
+
     let lhs = &RISTRETTO_BASEPOINT_TABLE * &signature.1;
     if lhs == rhs {
-        println!("\nSignature Verified : Success");
+        println!("#####################################\nSignature Verification Succeded \n#####################################");
         true
     } else {
-        println!(
-            "Signature Verification Failed: \n SHOULD BE :::::::::::: {:?} \n GOT :::::::::: {:?} \n",
-            rhs, lhs
-        );
+        println!("#####################################\nSignature Verification Failed \n#####################################");
+        // println!(
+        //     "Signature Verification Failed: \n SHOULD BE :::::::::::: {:?} \n GOT :::::::::: {:?} \n",
+        //     rhs, lhs
+        // );
         false
     }
 }
