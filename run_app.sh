a889af726a75fcd08cff3755f144d8cf8a45486c
l30 <thanhl@student.chalmers.se>
2023-05-02 19:56:37 +0200

    test commit with co-author
    Co-authored-by: tommiroy <tommiroy@users.noreply.github.com>

--- run_app.sh
+++ run_app.sh
@@ -7,3 +7,7 @@ xterm -hold -e 'cargo run client -i 1 -e docker_x509/ecu1/ecu1.pem -c docker_x50
 xterm -hold -e 'cargo run client -i 4 -e docker_x509/ecu4/ecu4.pem -c docker_x509/ca/ca.crt -a 127.0.0.1 -p 3034 --caddr central --cport 3030' &
 xterm -hold -e 'cargo run client -i 2 -e docker_x509/ecu2/ecu2.pem -c docker_x509/ca/ca.crt -a 127.0.0.1 -p 3032 --caddr central --cport 3030' &
 xterm -hold -e 'cargo run client -i 3 -e docker_x509/ecu3/ecu3.pem -c docker_x509/ca/ca.crt -a 127.0.0.1 -p 3033 --caddr central --cport 3030' &
+
+
+
+# Co-authored-by: tommiroy <tommiroy@users.noreply.github.com>
