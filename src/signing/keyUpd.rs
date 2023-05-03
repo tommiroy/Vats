b1f362166bf6215b83a65a6feef170e8d78feb8b
l30 <thanhl@student.chalmers.se>
2023-05-04 00:51:32 +0200

    done with verify new share

--- src/signing/keyUpd.rs
+++ src/signing/keyUpd.rs
@@ -28,30 +28,6 @@ pub fn verify_sigma(me: &Client, sigma: (RistrettoPoint, Scalar), context_string
     big_r == &RISTRETTO_BASEPOINT_TABLE * &zx + *me.pubkeys.get(&sender_id).expect("keyUpd-verify_sigma: Cannot find sender_id in pubkeys")*(-c)
 }
 
-pub fn verify_new_share(me: &mut Client, sender_id: u32, new_share: Scalar) {
-    let mut lhs = RistrettoPoint::identity();
-    if me.commitments.contains_key(&sender_id) {
-        // for (k, &big_a) in me.commitments.get(&sender_id).iter().enumerate() {
-        for (k, &big_a) in me.commitments.get(&sender_id).expect("keyUpd-verify_new_share: cannot get id").iter().enumerate() {
-
-            lhs += big_a*Scalar::from(sender_id)*Scalar::from(k as u32);
-        }
-    }
-
-    if &RISTRETTO_BASEPOINT_TABLE * &new_share == lhs {
-        me.set_share(me.get_share()+ new_share*compute_lagrange_coefficient(Committee::new(me.pubkeys.clone()), me.id));
-        me.pubkey = &RISTRETTO_BASEPOINT_TABLE * &me.get_share();
-        me.pubkeys.insert(me.id,me.pubkey);
-
-        // for 
-
-        // big_a*Scalar::from(sender_id)*Scalar::from(k as u32);
-        
-
-
-    }
-}
-
 pub fn update_pubkeys(me: &mut Client) {
     let com = Committee::new(me.pubkeys.clone());
     let mut new_pubkeys: HashMap<u32, RistrettoPoint> = HashMap::<u32, RistrettoPoint>::new();
@@ -75,7 +51,7 @@ pub fn update_pubkeys(me: &mut Client) {
 }
 
 
-pub async fn update_share(signer:&mut Client, participants: Vec<u32>, t: usize, context: String) -> bool{
+pub async fn update_share(signer:&mut Client, _participants: Vec<u32>, t: usize, context: String) -> bool{
     let mut rng: OsRng = OsRng;
 
     // Dealer samples t random values t-1 a   ----> t = 3
@@ -91,8 +67,8 @@ pub async fn update_share(signer:&mut Client, participants: Vec<u32>, t: usize, 
     // Dealer samples t random values t-1 a   ----> t = 3
 
     let mut new_shares = HashMap::<u32, Scalar>::new();
-    for i in participants.clone() {
-        let mut share = signer.get_share();
+    for &i in signer.pubkeys.keys() {
+        let mut share = Scalar::zero();
         for (j, &aj) in a.iter().enumerate() {
             share += aj * scalar_pow(Scalar::from(i as u32), j as u32);
         }
@@ -143,14 +119,55 @@ pub async fn update_share(signer:&mut Client, participants: Vec<u32>, t: usize, 
 
     // -------------------------- ROUND 2 ----------------------------------------
 
-    for participant in participants {
+    for &participant in signer.pubkeys.keys() {
         if new_shares.contains_key(&participant) {
-            let msg = Message {sender: signer.id.to_string(), receiver: participant.to_string(), msg_type: MsgType::KeyUpdNewShare, msg: vec![scalar_to_string(new_shares.get(&participant).expect("keyUpd: Cannot find participant"))]};
+            let msg: Message = Message {sender: signer.id.to_string(), receiver: participant.to_string(), msg_type: MsgType::KeyUpdNewShare, msg: vec![scalar_to_string(new_shares.get(&participant).expect("keyUpd: Cannot find participant"))]};
             signer.send("keyupd_newshare".to_string(), msg).await;
         }
+
+        let mut rhs = RistrettoPoint::identity();
+        for (k, &big_a) in big_as.iter().enumerate() {
+            rhs += big_a*scalar_pow(Scalar::from(participant), k as u32);
+        }
+
+
+        // warn!("Gen-lhs of {} for {}: {}", signer.id, participant.to_string(),  point_to_string(&RISTRETTO_BASEPOINT_TABLE * new_shares.get(&participant).unwrap()));
+        // warn!("Gen-rhs of {} for {}: {}", signer.id, participant.to_string(),  point_to_string(rhs));
+
+
     }
+
+
     // info!("Sent new shares");
 
     true
 
+}
+
+
+
+pub fn verify_new_share(me: &mut Client, sender_id: u32, new_share: Scalar) {
+    let mut rhs = RistrettoPoint::identity();
+    if me.commitments.contains_key(&sender_id) {
+        // for (k, &big_a) in me.commitments.get(&sender_id).iter().enumerate() {
+        // warn!("Found commitment from {sender_id}");
+        for (k, &big_a) in me.commitments.get(&sender_id).expect("keyUpd-verify_new_share: cannot get id").iter().enumerate() {
+            rhs += big_a*scalar_pow(Scalar::from(me.id), k as u32);
+        }
+
+    } else {
+        // warn!("Not Found commitment from {sender_id}");
+    }
+
+    // warn!("Verification-lhs of {}: {}",sender_id,  point_to_string(&RISTRETTO_BASEPOINT_TABLE * &new_share));
+    // warn!("Verification-rhs of {}: {}",sender_id,  point_to_string(rhs));
+
+    if &RISTRETTO_BASEPOINT_TABLE * &new_share == rhs {
+        me.set_share(me.get_share()+ new_share*compute_lagrange_coefficient(Committee::new(me.pubkeys.clone()), me.id));
+        me.pubkey = &RISTRETTO_BASEPOINT_TABLE * &me.get_share();
+        me.pubkeys.insert(me.id,me.pubkey);
+        info!("New Share from {sender_id} added");
+    } else {
+        info!("Cannot add new share from {sender_id}");
+    }
 }
